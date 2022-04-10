e089d8d9ca642c4b43369d0957955341d152b4a4
Alex <s-alex-smir@yandex.ru>
2022-04-10 19:58:18 +0600

    updates and fix

--- app.py
+++ app.py
@@ -4,11 +4,6 @@ from data.Standart import db_session
 from data_load import winddata_json, airtemp_json, winddata_max_min, airtemp_max_min, stations_main_p, stations_main_p2
 
 from data.database.station import Station
-from data.database.boiler import Boiler
-from data.database.objects import Objects
-from data.database.accounting_water import AccountingWater
-from data.database.accounting_warm import AccountingWarm
-from data.database.accounting_energy import AccountingEnergy
 
 
 db_session.global_init('db/database.db')
@@ -37,7 +32,45 @@ def object_info(station_id):
     db_sess = db_session.create_session()
     station = db_sess.query(Station).get(station_id)
 
-    return render_template('station-info.html', station=station)
+    json_data = dict()
+    colors = [
+        '#FB5156',
+        '#FBCB51',
+        '#77DDE7',
+        '#8579E2',
+        '#89E279',
+        '#919192',
+        '#E279CF',
+        '#003153',
+        '#8CCB5E',
+        '#79C7E2',
+        '#FFDB8B',
+        '#8A7F8E',
+        '#CED23A',
+        '#AC7580'
+    ]
+
+    for n, i in enumerate(station.boiler_unit_type):
+        if i.boiler_name not in json_data.keys():
+            json_data[i.boiler_name] = {}
+            json_data[i.boiler_name]['color'] = colors[n]
+
+        json_data[i.boiler_name]['2019_gas'] = i.fuel2019_gas
+        json_data[i.boiler_name]['2019_masut'] = i.fuel2019_masut
+        json_data[i.boiler_name]['2019_ugol'] = i.fuel2019_ugol
+        json_data[i.boiler_name]['2019_warm'] = i.warm2019
+
+        json_data[i.boiler_name]['2020_gas'] = i.fuel2020_gas
+        json_data[i.boiler_name]['2020_masut'] = i.fuel2020_masut
+        json_data[i.boiler_name]['2020_ugol'] = i.fuel2020_ugol
+        json_data[i.boiler_name]['2020_warm'] = i.warm2020
+
+        json_data[i.boiler_name]['2021_gas'] = i.fuel2021_gas
+        json_data[i.boiler_name]['2021_masut'] = i.fuel2021_masut
+        json_data[i.boiler_name]['2021_ugol'] = i.fuel2021_ugol
+        json_data[i.boiler_name]['2021_warm'] = i.warm2021
+
+    return render_template('station-info.html', station=station, json_data=json_data)
 
 
 if __name__ == '__main__':
